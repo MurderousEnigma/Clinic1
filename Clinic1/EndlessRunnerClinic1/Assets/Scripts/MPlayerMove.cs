0439412ce35802c02e0e48cbc76be93b89e6eb20
Mike Gauthier <mgauth12@student.fitchburgstate.edu>
2020-02-02 23:57:24 -0500

    Mike Scene
    Scene Added + Movement Added

---
+++ Clinic1/EndlessRunnerClinic1/Assets/Scripts/MPlayerMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MPlayerMove : MonoBehaviour
+{
+    private CharacterController pController;
+    public float movement = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pController = GetComponent<CharacterController>();
+    }
+
+    //continuously move player forward
+    void Update()
+    {
+        pController.Move((Vector3.forward * movement) *  Time.deltaTime);
+    }
+}
